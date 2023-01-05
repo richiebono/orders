d7e8b3f749a7d08df3e45792d914c5114aeb28b4
richiebono <richiebono@gmail.com>
2023-01-06 00:42:33 +0100

    Removed duplicated using using System.Linq;

--- orders-backend/src/1-Api/Bono.Orders.Api/Controllers/OrderController.cs
+++ orders-backend/src/1-Api/Bono.Orders.Api/Controllers/OrderController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
