a7024bce0118a00bfc96e3e6ce6d238783778cf1
richiebono <richiebono@gmail.com>
2023-01-10 10:59:03 +0100

    Updated metallb k8s.

--- infra/k8s/metallb/metallb.sh
+++ infra/k8s/metallb/metallb.sh
@@ -9,7 +9,13 @@ sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-frr.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f ./metallb-config.yaml
+kubectl wait --namespace metallb-system \
+                --for=condition=ready pod \
+                --selector=app=metallb \
+                --timeout=90s
 
+docker network inspect -f '{{.IPAM.Config}}' kind
+
+kubectl apply -f ./k8s/metallb/metallb-configmap.yaml -n metallb-system
+
+kubectl apply -f https://kind.sigs.k8s.io/examples/loadbalancer/metallb-config.yaml
